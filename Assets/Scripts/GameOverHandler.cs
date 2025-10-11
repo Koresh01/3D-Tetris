312d33f31ac71d3c0615122ce82e30355bcb25e0
Koresh01 <kmatveev2004@gmail.com>
2025-10-11 17:40:05 +0300

    Конец игры, когда падающая деталь вообще не попала на платформу.

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -8,9 +8,34 @@ public class GameOverHandler : MonoBehaviour
     [Header("Зависимости:")]
     [SerializeField] MenuController menuController;
 
+    [Header("Условия окончания игры:")]
+    [SerializeField] float Ybound;
+
     private void Update()
     {
+        HandleGridCanSpawn();
+        HandleActiveDetailZpos();
+    }
+
+    /// <summary>
+    /// Отслеживает конец игры, когда всё поле занято детальками.
+    /// </summary>
+    void HandleGridCanSpawn()
+    {
         if (!Grid.CanSpawn())
+            menuController.SetGameOverMode();
+    }
+
+    /// <summary>
+    /// Отслеживает ситуацию, когда падающая деталь вообще не попала на поле.
+    /// </summary>
+    void HandleActiveDetailZpos()
+    {
+        GameObject currentDetail = GameManager.currentDetail;
+        if (currentDetail == null) return;
+
+        float yPos = currentDetail.transform.position.y;
+        if (yPos < Ybound)
             menuController.SetGameOverMode();
     }
 }
